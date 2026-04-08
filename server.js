3edc6f772cb15138e2c4e10e6ca6ef822106f916
Ignite Studio <ignitestudio303@gmail.com>
2026-04-07 19:45:20 -0600

    v10
    mockup id

--- server.js
+++ server.js
@@ -11,20 +11,34 @@ app.get('/', (req, res) => {
 
 // Mockups route
 
-app.get('/mockups', async (req, res) => {
+app.get('/render', async (req, res) => {
   try {
-    const response = await axios.get('https://app.dynamicmockups.com/api/v1/mockups', {
-      headers: {
-        'x-api-key': process.env.DYNAMIC_API_KEY,
-        'Accept': 'application/json'
+    const response = await axios.post(
+      'https://app.dynamicmockups.com/api/v1/renders',
+      {
+        mockup_uuid: "f0d59740-e127-4086-b122-847de1b0e389",
+        smart_objects: [
+          {
+            uuid: "bdb4f385-9648-4a65-8214-48c5eb07c733",
+            asset: {
+              type: "image",
+              url: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Fronalpstock_big.jpg"
+            }
+          }
+        ]
+      },
+      {
+        headers: {
+          'x-api-key': process.env.DYNAMIC_API_KEY,
+          'Content-Type': 'application/json'
+        }
       }
-    });
+    );
 
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(response.data);
+    res.json(response.data);
 
   } catch (error) {
-    console.error('API ERROR:', error.response?.data || error.message);
+    console.error('RENDER ERROR:', error.response?.data || error.message);
 
     res.status(500).json({
       error: error.response?.data || error.message
